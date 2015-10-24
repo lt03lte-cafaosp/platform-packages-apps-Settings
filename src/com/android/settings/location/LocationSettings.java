295adfdf413232ca236689162df4b7b52beed9e9
Kevin Tang <zhikait@codeaurora.org>
2015-10-23 19:57:57 -0700

    Revert "Show EULA consent option based on presence of GMS"
    This reverts commit f7129a7db4b4f0995b9704d46277a5e55d70854a.
    CRs-Fixed: 929695
    
    Change-Id: I657ea976634aba778595397e5bbcf2fb94b462b0

--- src/com/android/settings/location/LocationSettings.java
+++ src/com/android/settings/location/LocationSettings.java
@@ -253,7 +253,7 @@ public class LocationSettings extends LocationSettingsBase
             boolean lockdownOnLocationAccess) {
         PreferenceCategory categoryLocationServices =
                 (PreferenceCategory) root.findPreference(KEY_LOCATION_SERVICES);
-        injector = IzatSettingsInjector.getSettingInjector(context);
+        injector = new SettingsInjector(context);
         // If location access is locked down by device policy then we only show injected settings
         // for the primary profile.
         List<Preference> locationServices = injector.getInjectedSettings(lockdownOnLocationAccess ?
