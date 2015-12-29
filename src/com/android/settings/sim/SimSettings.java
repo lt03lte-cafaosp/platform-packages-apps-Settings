8a2ed29686a89c5871448701c681ceb913378236
Rakesh Pallerla <rakesh@codeaurora.org>
2015-12-29 17:38:10 +0530

    UI Changes for RJIL Primary card Feature
    1. Do not show Switch LTE Data Dialog if number of
       active sub is less than one.
    2. Re-enable the Primary card preference if required.
    3. Add mcc,mnc to config changes so that activity is
       not re-created on config changed.
    
    Change-Id: I1540e643e517113bcd5148efa1fdc1fc9cae5606
    CRs-Fixed: 956483, 956618, 956494

--- src/com/android/settings/sim/SimSettings.java
+++ src/com/android/settings/sim/SimSettings.java
@@ -92,6 +92,7 @@ public class SimSettings extends RestrictedSettingsFragment implements Indexable
     private List<SubscriptionInfo> mAvailableSubInfos = null;
     private List<SubscriptionInfo> mSubInfoList = null;
     private Preference mPrimarySubSelect = null;
+    private boolean mPrimaryPrefRemoved = false;
 
     private static List<MultiSimEnablerPreference> mSimEnablers = null;
 
@@ -429,8 +430,16 @@ public class SimSettings extends RestrictedSettingsFragment implements Indexable
         if (!isPrimarySubFeatureEnable || !primarySetable) {
             final PreferenceCategory simActivities =
                     (PreferenceCategory) findPreference(SIM_ACTIVITIES_CATEGORY);
-            simActivities.removePreference(mPrimarySubSelect);
+            if (!mPrimaryPrefRemoved) {
+                simActivities.removePreference(mPrimarySubSelect);
+                mPrimaryPrefRemoved = true;
+            }
             return;
+        } else if (mPrimaryPrefRemoved == true) {
+            final PreferenceCategory simActivities =
+                    (PreferenceCategory) findPreference(SIM_ACTIVITIES_CATEGORY);
+            simActivities.addPreference(mPrimarySubSelect);
+            mPrimaryPrefRemoved = false;
         }
 
         int primarySlot = getCurrentPrimarySlot();
@@ -528,6 +537,7 @@ public class SimSettings extends RestrictedSettingsFragment implements Indexable
                         int phoneId = mSubscriptionManager.getPhoneId(defaultDataSubId);
 
                         if (isDdsSwitchAlertDialogSupported(defaultDataSubId) &&
+                                subSelectableSize > 1 &&
                                ((mVoiceNetworkType[phoneId] == TelephonyManager.NETWORK_TYPE_LTE) |
                                 (mDataNetworkType[phoneId] == TelephonyManager.NETWORK_TYPE_LTE))) {
                             Log.d(TAG, "DDS switch request from LTE sub");
