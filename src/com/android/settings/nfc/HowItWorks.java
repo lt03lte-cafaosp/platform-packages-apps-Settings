7d9cd4964417e15b9775c781675ec9bba54e863a
yumeng <yumeng@codeaurora.org>
2016-06-06 11:08:57 +0800

    Settings: "Tap & pay" display incpmplete in landscape.
    Load another XML to fit the screen when do the landscape screen.
    
    Change-Id: If8ad3a8e6e37f9d31cd3635f3ffff69fefce41c6
    CRs-Fixed: 1003904

--- src/com/android/settings/nfc/HowItWorks.java
+++ src/com/android/settings/nfc/HowItWorks.java
@@ -1,6 +1,7 @@
 package com.android.settings.nfc;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +11,12 @@ public class HowItWorks extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.nfc_payment_how_it_works);
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.nfc_payment_how_it_works_for_lanscape);
+        } else {
+            setContentView(R.layout.nfc_payment_how_it_works);
+        }
         getActionBar().setDisplayHomeAsUpEnabled(true);
         Button gotIt = (Button) findViewById(R.id.nfc_how_it_works_button);
         gotIt.setOnClickListener(new View.OnClickListener() {
