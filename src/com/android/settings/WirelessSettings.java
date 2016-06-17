ff892a8127b13e03b215c813df1798b44b99567c
yumeng <yumeng@codeaurora.org>
2016-06-17 17:35:13 +0800

    Settings: adjust to the design for RJIL mode.
    add some preferences to adjust to the view design for RJIL..
    
    Change-Id: Ibedf154328108e6e24274ffb0be07ebb23a81745
    CRs-Fixed: 1037576

--- src/com/android/settings/WirelessSettings.java
+++ src/com/android/settings/WirelessSettings.java
@@ -334,8 +334,13 @@ public class WirelessSettings extends SettingsPreferenceFragment implements Inde
         // Disable Tethering if it's not allowed or if it's a wifi-only device
         final ConnectivityManager cm =
                 (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        boolean isShowTethering = activity.getResources().getBoolean(R.bool.config_settings_design);
+        if (isShowTethering) {
+            removePreference(KEY_MOBILE_NETWORK_SETTINGS);
+        }
         if (isSecondaryUser || !cm.isTetheringSupported()
-                || mUm.hasUserRestriction(UserManager.DISALLOW_CONFIG_TETHERING)) {
+                || mUm.hasUserRestriction(UserManager.DISALLOW_CONFIG_TETHERING) 
+                || isShowTethering) {
             getPreferenceScreen().removePreference(findPreference(KEY_TETHER_SETTINGS));
         } else {
             Preference p = findPreference(KEY_TETHER_SETTINGS);
